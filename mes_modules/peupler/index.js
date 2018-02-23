5bbee7b256b4a0d3d1c18ddc0c3af62484c338a4
Yan Nelson-Lapointe <yannl97@hotmail.com>
2018-02-23 13:30:07 -0500

    ajout du dossier mes_modules

---
+++ mes_modules/peupler/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const tableau = require("./tableau.js");
+
+const maxNom = tableau.tabNom.length;
+const maxPrenom = tableau.tabPrenom.length;
+const maxTelephone = tableau.tabTelephone.length;
+const maxCourriel = tableau.tabCourriel.length;
+
+
+const peupler = () => {
+	console.log("ok");
+	let collection = [];
+	for (let i=0;i<10;i++){
+		let positionN = Math.floor(Math.random()*maxNom);
+		let nom = tableau.tabNom[positionN];
+
+		let positionP = Math.floor(Math.random()*maxPrenom);
+		let prenom = tableau.tabPrenom[positionP];
+
+		let positionT = Math.floor(Math.random()*maxTelephone);
+		let telephone = tableau.tabTelephone[positionT];
+
+		let positionC = Math.floor(Math.random()*maxCourriel);
+		let courriel = tableau.tabCourriel[positionC];
+
+		collection.push({
+			courriel : prenom+nom+courriel,
+			telephone : telephone,
+			prenom : prenom,
+			nom : nom
+		})
+	}
+	return collection;
+}
+
+module.exports = peupler;
---
+++ mes_modules/peupler/tableau.js
@@ -0,0 +1,89 @@
+/*"use strict";
+const tabVille = [
+					"Montréal",
+					"Ottawa",
+					"Laval",
+					"Paris",
+					"Bruxelle",
+					"Gatineau",
+					"Gaspé",
+					"Calgary",
+					"Saskatshewan"
+				];
+
+const tabDomaine = [
+					"gmail.com",
+					"videotron.ca",
+					"bell.ca",
+					"yahoo.com",
+					"hotmail.com"
+				];
+
+const tableau = {
+	tabVille : tabVille,
+	tabDomaine : tabDomaine 
+
+}
+
+/*
+module.exports.tabVille = tabVille;
+module.exports.tabDomaine = tabDomaine;	
+*/
+const tabNom = [
+	"Ouellet",
+	"Béland",
+	"Fréchette",
+	"Racicot",
+	"Chouinard",
+	"Davignon",
+	"Breton",
+	"Lacasse",
+	"Tardif", 
+	"Ruest" 
+];
+
+const tabPrenom = [
+	"Franck",
+	"Denis",
+	"Lance",
+	"Étienne",
+	"Yves",
+	"Martine",
+	"Jacqueline",
+	"Nathalie",
+	"Élisabeth",
+	"Sylvie"
+];
+
+const tabTelephone = [
+	"514-931-4943",
+	"514-720-1003",
+	"514-229-9692",
+	"514-573-0021",
+	"514-638-6974",
+	"514-644-7124",
+	"514-435-5178",
+	"514-583-7838",
+	"514-922-4752",
+	"514-846-3822"
+];
+
+const tabCourriel = [
+	"@hotmail.com",
+	"@gmail.com",
+	"@videotron.ca",
+	"@bell.ca",
+	"@yahoo.com",
+	"@sympatico.fr",
+	"@outlook.com"
+];
+
+const tableau = {
+	tabNom : tabNom,
+	tabPrenom : tabPrenom,
+	tabTelephone : tabTelephone,
+	tabCourriel : tabCourriel 
+
+}
+
+module.exports = tableau;						
